c6f18449d44b10a4b8d5697ceb40af0cf9d901ba
dedanirungu <dedanirungu@gmail.com>
2017-08-22 09:23:28 +0300

    Adding tooltips to all setting fields and addition of double authentication

---
+++ Doubleauth/Routes/Code/Tables/Routes.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Users\Doubleauth\Routes\Code\Tables;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Routes
+ *
+ * @ORM\Table(name="users_doubleauth_routes")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Routes extends \Kazist\Table\BaseTable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="route", type="string", length=255, nullable=false)
+     */
+    protected $route;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="published", type="integer", length=11, nullable=true)
+     */
+    protected $published;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="created_by", type="integer", length=11, nullable=true)
+     */
+    protected $created_by;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_created", type="datetime", nullable=true)
+     */
+    protected $date_created;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="modified_by", type="integer", length=11, nullable=true)
+     */
+    protected $modified_by;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_modified", type="datetime", nullable=true)
+     */
+    protected $date_modified;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set route
+     *
+     * @param string $route
+     *
+     * @return Routes
+     */
+    public function setRoute($route)
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    /**
+     * Get route
+     *
+     * @return string
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Set published
+     *
+     * @param integer $published
+     *
+     * @return Routes
+     */
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get published
+     *
+     * @return integer
+     */
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return integer
+     */
+    public function getCreatedBy()
+    {
+        return $this->created_by;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return \DateTime
+     */
+    public function getDateCreated()
+    {
+        return $this->date_created;
+    }
+
+    /**
+     * Get modifiedBy
+     *
+     * @return integer
+     */
+    public function getModifiedBy()
+    {
+        return $this->modified_by;
+    }
+
+    /**
+     * Get dateModified
+     *
+     * @return \DateTime
+     */
+    public function getDateModified()
+    {
+        return $this->date_modified;
+    }
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        // Add your code here
+    }
+}
+
