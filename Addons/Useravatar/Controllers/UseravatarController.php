48450af14107469f83a25b4403eb1d0e592a7046
Dedan Irungu <dedanirungu@gmail.com>
2018-08-06 04:38:03 +0300

    Changes to add ways to call useravatar via url

--- Addons/Useravatar/Controllers/UseravatarController.php
+++ Addons/Useravatar/Controllers/UseravatarController.php
@@ -11,26 +11,48 @@ namespace Users\Addons\Useravatar\Controllers;
 defined('KAZIST') or exit('Not Kazist Framework');
 
 use Kazist\Controller\AddonController;
-use Users\Addons\Useravatar\Models\UseravatarModel;
+use Kazist\KazistFactory;
 
 /**
  * Kazist view class for the application
  *
  * @since  1.0
  */
-class UseravatarController extends AddonController {
+class UseravatarController extends AddonController
+{
 
-    function indexAction($show_inviter_link = false, $my_account_link = 'users.users.edit') {
+    public function indexAction($show_inviter_link = false, $my_account_link = 'users.users.edit')
+    {
+
+        $factory = new KazistFactory();
 
         $data_arr['show_inviter_link'] = $show_inviter_link;
         $data_arr['my_account_link'] = $my_account_link;
 
+        $notheme = $this->request->get('notheme');
+        $useremail = $this->request->get('useremail');
+
+        if ($useremail != '') {
+            $user = $factory->getRecord('#__users_users', 'uu', array('email=:email'), array('email' => $useremail));
+
+            $data_arr['user_id'] = $user->id;
+        }
 
         $this->html = $this->render('Users:Addons:Useravatar:views:useravatar.twig', $data_arr);
 
         $response = $this->response($this->html);
 
+        if ($notheme == '') {
+            $response = $this->response($this->html);
+        } else {
 
+            http_response_code(200);
+
+            header('Content-Type: application/json');
+
+            echo json_encode(array('html' => $this->html));
+            exit;
+        }
 
         return $response;
     }
